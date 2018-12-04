8b5db30786bd6c00b77292abecbc903925e3a733
Archit Raichura <archit.raichura@gmail.com>
2018-12-04 20:00:28 +0000

    updated files

--- cars database.sql
+++ cars database.sql
@@ -3,8 +3,34 @@ use cars;
 create table cars(
 yr varchar(4),
 typ varchar(10),
-fuel varchar(10),
 manufacturer varchar(20),
 model varchar(20),
-cost varchar(10))
+cost varchar(10));
+
+INSERT INTO cars (yr, typ, manufacturer, model, cost)
+VALUES ('2012', 'SUV', 'Toyota', 'Highlander', '3200'),
+('1993', 'Sedan', 'Honda', 'Accord', '700'),
+('2008', 'SUV', 'Land Rover', 'Range Rover', '11000'),
+('2012', 'SUV', 'Kia', 'Soul', '5200'),
+('2017', 'SUV', 'Nissan', 'Armada', '28888'),
+('2012', 'Coupe', 'Nissan', 'GT-R', '53800'),
+('2013', 'Hatchback', 'Subaru', 'WRX', '18995'),
+('2016', 'Sedan', 'Toyota', 'Corolla S', '14995'),
+('2010', 'Truck', 'Toyota', 'Tacoma', '17995'),
+('2007', 'Sedan', 'Honda', 'Civic', '4000'),
+('2004', 'Sedan', 'Honda', 'Accord', '2000'),
+('1987', 'Coupe', 'Chevrolet', 'Corvette', '4999'),
+('2007', 'Sedan', 'Audi', 'A4 Quattro', '5500'),
+('1970', 'Truck', 'Ford', 'Bronco', '42000'),
+('1992', 'Coupe', 'Pontiac', 'Firebird Formula 5.0', '3500'),
+
+('2019','Coupe','Ferrari','488','252800'),
+('2017','Coupe','Chevrolet','Camaro SS','27988'),
+('2008','Coupe','Nissan','350Z 6Speed Manual','12000'),
+('1967','Coupe','Volvo','122s','8000'),
+('2013','Hatchback','Volkswagen','Golf GTI','12800'),
+('2005','Coupe','Dodge','Neon SXT','2100'),
+('2015','Hatchback','Mini','Cooper Countryman','16350')
+;
+
 
--- phpTest.php
+++ phpTest.php
@@ -1,18 +1,30 @@
+
+<table>
+    <tr>
+        <th>Year</th>
+        <th>Type</th>
+        <th>Make</th>
+        <th>Model</th>
+        <th>Cost ($)</th>
+    </tr>
+    
 <?php
     
     //Connect to the database
     $host = "127.0.0.1";
-    $user = "your_username";                     //Your Cloud 9 username
+    $user = "architr";                     //Your Cloud 9 username
     $pass = "";                                  //Remember, there is NO password by default!
-    $db = "c9";                                  //Your database name you want to connect to
+    $db = "cars";                                  //Your database name you want to connect to
     $port = 3306;                                //The port #. It is always 3306
     
     $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
 
 //And now to perform a simple query to make sure it's working
-    $query = "SELECT * FROM users";
+    $query = "SELECT * FROM cars";
     $result = mysqli_query($connection, $query);
 
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "The ID is: " . $row['id'] . " and the Username is: " . $row['username'];
+        echo "\t<tr><td>".$row['yr']."</td><td>".$row['typ']."</td><td>".$row['manufacturer']."</td><td>".$row['model']."</td><td>".$row['cost']."</td></tr>\n";
     }
+    ?>
+</table>
