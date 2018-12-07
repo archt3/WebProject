d14750c30b8917792dc7328fb7b78be13992a57c
Archit Raichura <archit.raichura@gmail.com>
2018-12-07 19:46:02 +0000

    formtest

--- phptest2.php
+++ phptest2.php
@@ -1,4 +1,4 @@
-<table>
+<table id="cars">
     <tr>
         <th>Year</th>
         <th>Type</th>
@@ -18,26 +18,28 @@
 		case 'order by cost;':$query = "SELECT * FROM cars ORDER BY CAST(cost as SIGNED INTEGER) ASC;";
 				//echo $query;
 				$result = mysqli_query($conn, $query);
+				
 				while ($row = mysqli_fetch_assoc($result)) {
 					echo "\t<tr><td>".$row['yr']."</td><td>".$row['typ']."</td><td>".$row['manufacturer']."</td><td>".$row['model']."</td><td>".$row['cost']."</td></tr>\n"; 
 					
 				} 
+				break;
 				$result->close();
 					$conn->close();
 		
 	
 		default:
+			
 			if ($option){
 				$testField = $_POST['myTest'];
 				//echo $testField;
 				$query = "	select ". $testField . " from cars;";
-				//echo $query;
+				
 				$result = mysqli_query($conn, $query);
 				while ($row = mysqli_fetch_assoc($result)) {
 					echo "<br>";
-				echo $row[$testField] ;
-				
-			}
+				echo $row[$testField];
+				}
 		}}
 		
 		
