badf8a4b50507a43e0e9086f2a8ef0a8b0022212
Archit Raichura <archit.raichura@gmail.com>
2019-01-11 19:36:30 +0000

    comments added

--- phptest2.php
+++ phptest2.php
@@ -20,14 +20,13 @@
 		if($conn->connect_error) die($conn->connect_error);
 		$option = isset($_POST['myTest']) ? $_POST['myTest'] : false; //from the sample
 	
-$query =$_POST['myTest']; 
-				//Select all, order by cost increasing
+$query =$_POST['myTest']; //Set the query to whatever the post is; the posts are all the various queries AR
 				$result = mysqli_query($conn, $query);
 				
 				while ($row = mysqli_fetch_assoc($result)) {
 					
 					echo  "\t<tr><td>".$row['yr']."</td><td>".$row['typ']."</td><td>".$row['manufacturer']."</td><td>".$row['model']."</td><td>".$row['cost']."</td></tr>\n".'</span>'; 
-					
+					//the echo is to format in a table AR
 				} 
 				
 				$result->close();
